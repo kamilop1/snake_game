9cdd61b84897cb3057fbcad66d4b0e435024a84f
Kamil <kamilrayman1@gmail.com>
2022-07-11 20:34:19 +0200

    fixed movement and overflow

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const main = document.querySelector(".main")
-const cols = 22;
+const cols = 22; //settings:
 const rows = 22;
-const game_speed = 200;
-let snek;
+const game_speed = 400;
+
+
+let snek, push;
 let current_direction = "right";
+let running = false;
 
 window.addEventListener("load", evt => {grid_setup()} )
 const grid_setup = () => {
+    const main = document.querySelector(".main")
     main.setAttribute("style",`grid-template-columns: repeat(${cols},1fr); grid-template-rows: repeat(${rows},1fr)`)
     for(let i=0;i<cols*rows;i++){
         let div = document.createElement("div");
@@ -14,13 +17,18 @@ const grid_setup = () => {
         main.appendChild(div);
     }
     let center_ish=rows*Math.floor(cols/2)+Math.floor(cols/3)
-    snek = new Snake([center_ish,center_ish+1,center_ish+2]);
+    snek = new Snake([center_ish,center_ish+1,center_ish+2,center_ish+3,center_ish+4]); // default snake
     snek.update()
-    document.body.addEventListener("keydown", evt => {snek.detection()})
-    setInterval(() => {
-        snek.move(snek.current_direction);
-        snek.update();
-    }, game_speed);
+    snek.detection();
+    document.body.addEventListener("keydown", evt => {
+        if(running==false){
+            setInterval(() => {
+                snek.move(snek.current_direction);
+                snek.update();
+            }, game_speed);
+            running = true 
+        }
+    })
 }
 class Snake{
     current_direction = "right";
@@ -31,20 +39,28 @@ class Snake{
         this.chunks.shift();
         switch(direction){
             case "up":
-                this.chunks.push(this.chunks[this.chunks.length-1]-rows);
-                console.log("movin up")
+                push = this.chunks[this.chunks.length-1]-rows
+                if(push < 0){push += cols*rows}
+                this.chunks.push(push);
                 break;
             case "down":
-                this.chunks.push(this.chunks[this.chunks.length-1]+rows);
-                console.log("movin down")
+                push = this.chunks[this.chunks.length-1]+rows
+                if(push > cols*rows){push -= cols*rows}
+                this.chunks.push(push);
                 break;
             case "left":
-                this.chunks.push(this.chunks[this.chunks.length-1]-1);
-                console.log("movin left")
+                push = this.chunks[this.chunks.length-1]
+                if(push%cols == 0){
+                    push += cols
+                }
+                this.chunks.push(push-1);
                 break;
             case "right":
-                this.chunks.push(this.chunks[this.chunks.length-1]+1);
-                console.log("movin right")
+                push = this.chunks[this.chunks.length-1]
+                if((push+1)%cols == 0){
+                    push -= cols
+                }
+                this.chunks.push(push+1);
                 break;
         }
         snek.update();
